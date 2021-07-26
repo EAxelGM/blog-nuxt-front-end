9cdfb999f1d9f3ae76a2f2ce8cb41a89f4f3e5d8
EAxelGMExcite <axel@excite.com.mx>
2021-07-25 19:36:24 -0500

    posts

---
+++ components/Posts/EsqueletoMuchosPost.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-skeleton-loader
+      v-bind="attrs"
+      type="article, image, actions"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      attrs: {
+        class: 'mb-6',
+        boilerplate: true,
+        elevation: 2,
+      },
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
-    <v-row>
+    <v-row justify="center" v-if="fullPaginacion > 0">
+        <v-col cols="8">
+          <v-container class="max-width" @click="getPosts()">
+            <v-pagination
+              v-model="pagina"
+              class="my-4"
+              :total-visible="6"
+              circle
+              :length="fullPaginacion"
+            />
+          </v-container>
+        </v-col>
+      </v-row>
+
+    <v-row v-if="!loading">
       <v-col v-for="(post, index) in posts" :key="index" cols="12" md="4">
         <ViewPost :post="post" />
+      </v-col>
+    </v-row>
+
+    <v-row v-else>
+      <v-col v-for="(number, index) in 9" :key="index" cols="12" md="4">
+        <EsqueletoLoading />
       </v-col>
     </v-row>
 
@@ -24,9 +44,11 @@
 
 <script>
 import ViewPost from '@/components/Posts/CardPosts';
+import EsqueletoLoading from '@/components/Posts/EsqueletoMuchosPost';
 export default {
   components:{
     ViewPost,
+    EsqueletoLoading
   },
   layout: 'logged',
   data(){
@@ -34,13 +56,17 @@ export default {
       pagina: 1,
       fullPaginacion: 0,
       posts: [],
+      firsLoading: true,
+      loading: false,
     }
   },
   mounted(){
     this.getPosts();
+    this.firstLoading = false;
   },
   methods:{
     async getPosts(){
+      this.loading = true;
       const url = `post?page=${this.pagina}`
       try {
         const {data} = await this.$axios.get(url);
@@ -49,6 +75,7 @@ export default {
       } catch (error) {
         console.log({error});
       }
+      this.loading = false;
     }
   }
 }
